4fa81cfc53bea6253e27bdddf203822baacaf510
Rohan Vashisht <38130791+RohanVashisht003@users.noreply.github.com>
2020-04-08 11:55:09 +0530

    Update Print Farnehite To Celcius Table.cpp

--- Print Farnehite To Celcius Table.cpp
+++ Print Farnehite To Celcius Table.cpp
@@ -5,11 +5,9 @@ using namespace std;
      int initi_value, fin_value, step;
      cin>>initi_value>>fin_value>>step;
 
-     for(int i=initi_value; i<=fin_value;)
+     for(int i=initi_value; i<=fin_value;  i+step)
      {
          int c=(5/9.0)*(i-32);
-
-         i=i+step;
 
          cout<<i<<" "<<c<<endl;
 
