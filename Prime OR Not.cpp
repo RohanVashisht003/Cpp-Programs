be6793dffcaff51adeefb13adb1be2b38fb901c3
Rohan Vashisht <38130791+RohanVashisht003@users.noreply.github.com>
2020-04-14 23:10:20 +0530

    Create Prime OR Not.cpp
    Input
    4
    
    Output
    Not Prime

---
+++ Prime OR Not.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n,counter=0;
+cin>>n;
+
+for(int i=1; i<=n; i++)
+{
+  if(n%i==0)
+  {
+      counter++;
+  }
+}
+
+if(counter==2)
+{
+    cout<<"Prime";
+}
+else
+{
+cout<<"Not Prime";
+}
+}
+
